cf150addb9cb930e4370e04f7040e8de85c7643c
Zero <johanjimenez0210@gmail.com>
2024-03-05 15:06:22 -0500

    <Fix> Cors

--- TechTrial/settings.py
+++ TechTrial/settings.py
@@ -135,4 +135,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "https://prueba-frontend-cjt4.vercel.app"
+]
